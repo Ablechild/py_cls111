c74229a0830efc99e082b396475c1eb340674c0e
Gideon Joseph <141260742+Ablechild@users.noreply.github.com>
2023-12-15 14:11:59 +0100

    first commit

---
+++ exp2.py
@@ -0,0 +1,24 @@
+# x = 1
+# if x < 10 :
+#     print ('Smaller')
+# if x >20 :
+#     print ('Bigger')
+# print('Finish')
+
+
+
+# 
+
+x =5
+if x ==5 :
+    print('is 5')
+    print ('is still 5')
+    print('third 5')
+print('afterwards 5')
+
+x = 6
+if x == 6 :
+    print('is 6')
+    print ('is still 6')
+    print('third 6')
+print('afterwards 6')
